82d2eb46dbc8ed5ae73d0ad53caedea4c88fb934
Your Name <you@example.com>
2024-11-22 10:09:03 +0200

    PD-WEB

---
+++ basics.js
@@ -0,0 +1,22 @@
+let personAge = 17;
+if (personAge < 18){
+    console.log("Nepilngadīgs!")
+}
+if (personAge >= 18){
+    console.log("Pilngadīgs!")
+}
+
+
+const namesArray = ["Jānis", "Anna", "Zaiga"];
+
+for (let i = 3; i <= namesArray.length; i++){
+    console.log(namesArray)
+}
+
+
+
+function sayHello() {
+    console.log("Sveiks,Rinald")
+}
+
+sayHello()
